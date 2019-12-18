ef45432f7bf55a40aebd038201e8b10ddbc66404
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-12-18 12:59:45 +0100

    AJAX Part is not updated version and won't be in a nearest time

--- 6._JS_JSON_AJAX/javas/ajax.js
+++ 6._JS_JSON_AJAX/javas/ajax.js
@@ -3,14 +3,13 @@ document.getElementById("but1").addEventListener("click", myFun);
 
 function myFun() {
     console.log("HELLO");
-    let desc = new XMLHttpRequest();
+    var desc = new XMLHttpRequest();
     desc.onreadystatechange = function () {
         if (desc.readyState == 4 && desc.status == 200) {
             document.getElementById('output4').innerHTML = this.responseText;
         }
     }
-    d.open("GET", "data.txt", true);
-    d.send();
+    desc.open("GET", "data.php", true);
+    desc.send();
 }
 
-// AJAX Server interaction.117 - update form to multiple inputs challenge 122 - przerwa do uzyskania odpowiedzi.
---
+++ 6._JS_JSON_AJAX/javas/data.php
@@ -0,0 +1,6 @@
+<?php
+// Methodes written in php as you can see the course is not too updated - sorry
+echo $_POST['name'] . " " .  $_POST['age'];
+// echo $_GET['name'];
+// echo date('l js \of F Y h:i:s A')
+?>
