802e82fe0997af5ef7980e048380be1af7466b58
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-12-20 22:57:58 +0100

    Traversing with jQuery finished

--- 7._JQuery/7.3-Traversing/javas/2.relations.js
+++ 7._JQuery/7.3-Traversing/javas/2.relations.js
@@ -1,76 +1,10 @@
-############################################################################ JQuery - Traversing #######################
-############################################################
-
-# X. Co to jest trawersowanie
-
-- Traversing \* jest to związek/zależności pomiędazy DOM a jQuery elementami w bloku kodu <b>HTML</b>
-
-### Ćwiczenia
-
-- Formularz do tego ćwiczenia znajduje się w pliku traversing.html
-
-### // Praygotować dokument
-
-\$(document).ready(function () {
-
-### // Przygotowujemy guzik, który sprawdzać będzie nam walidację formulrza
-
-    $('#check').click(checkme);
-
-### // Przypisaliśmy do niego funkcję, którą zdefiniujemy ponieżej
-
-function checkme(e) {
-
-### //deklaracja pustego errora
-
-let err = "Fill the field correctly";
-
-### //przypisanie do zmiennej wywołania eventu
-
-const mytarget = \$(e.currentTarget);
-
-### //Deklaracja przeszukiwania inputów do walidacji, ale gdy dodamy jeszcze textarea to będzie już alidował wszystko, na razie tylko czy puste czy nie ponieważ nie stworzyliśmy np regEx'a do walidacji
-
-        $('#myForm').find('input, textarea').each(function (e) {
-
-### // Dopiszmy sobie jeszcze wyświetlanie errora o tym czego brakuje nam w polach do wypełnienia a mianowicie inputah
-
-if ($(this).val() === '') { err = err + "<br>needs " + $(this).attr('id'); }
-
-### // Sprawdzenia konsolowe informacji wyświetlanej
-
-### //Tyczy się tylko wartości zadeklarowanych w inputach poniważ nie wzięliśmy pod uwagę textarea.
-
-// console.log(\$(this).attr('id'))
-
-### // Wyświetlanie w konsoli tego co wpisAne do inputa
-
-            // console.log(e + ' ' + $(this).val())
-
-});
-
-### // Warunek wyświetlania inputa
-
-if (err.length > 0) {
-
-### // Przypisanie errora do miejsca na stronie
-
-$('#errors').html(err);
-        } else {
-            $('#output').html('form ok!')
-}
-}
-})
-
-## Ćwiczenia Rodzic, Dziecko, Rodzeństwo.
-
 // Rodzice - Parents, Dzieci - Childrens, Rodzeństwo - Siblings, czyli zależności hierarchiczne w HTML'u i jak to się robi za pomocą jQuery.
 
 // Przygotuj dokument
 $(document).ready(function () {
     //wybieżmy rodzica elemtntu/ parqaqgrafu numer childtwo, parent - daje najbliższego rodzica, parents daje wszystkich starszych
     console.log($('.childtwo').parent());
-console.log(\$('.childtwo').parents());
+    console.log($('.childtwo').parents());
 
     // Teraz patrzymy na dzieci i zmieńmy ich text
     $('.parentone').children().html('<p>Child</p>');
