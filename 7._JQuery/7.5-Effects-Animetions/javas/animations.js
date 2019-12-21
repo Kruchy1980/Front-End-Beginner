064b94b3f1c6db8fc230562862c6f38c32ba7b50
Kruchy1980 <jakub.kruszynski@vp.pl>
2019-12-21 18:03:02 +0100

    JQuery CSS finished  + Initial of Jquery Asynchronimus requests

--- 7._JQuery/7.5-Effects-Animetions/javas/animations.js
+++ 7._JQuery/7.5-Effects-Animetions/javas/animations.js
@@ -1,14 +1,25 @@
-// Animcje
+// Animcje - animate()
 // Przygotuj dokument
 $(document).ready(function () {
-    // Przygotoweane z poprzedniej części 
-    $('img:first').click(function () {
-        let mySrc = $(this).attr('src');
-        $('#output1').html(mySrc);
-    });
-    $('img:last').click(function () {
-        let mySrc = $(this).attr('src');
-        $('#output2').html('Kliknij mnie: ' + mySrc);
-    });
+    // Teraz chcemy dodać animację
+    // Ma ona bardzo dużo opcji, a zapisujemy ją następująco: 
+    // animate({
+    // attr1: value1,
+    //     attr2: value2,
+    //     ...: ...,
+    // attr(n): value(n)
+    // }, time - który mówi jak długo animacja ma się wykonywać, i możemy dalej dodać np function () {})
+    $('#mySlider').css({ 'position': 'absolute', 'top': '300px' });
+    $('#mySlider').on('click', function () {
+        $(this).animate({
+            opacity: 0.5,
+            left: "+=50px",
+            top: "-=10px",
+            heigth: '500',
+            width: '750px'
+        }, 2000, function () {
+            $('#output1').html('Animate working');
+        });
+    })
 
 })
